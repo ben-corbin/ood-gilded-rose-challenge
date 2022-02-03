fbfb403c2b799860ae1f1cc4c6352e1f1a415e0c
Ben Corbin <ben@corbin.digital>
2022-02-03 11:20:38 +0000

    More variables extracted, if statements combined. Thinking about class inheritance to extend items for each of the different object types

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,44 +10,45 @@ const BRIE = 'Aged Brie'
 const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert'
 const SULFURAS = 'Sulfuras, Hand of Ragnaros'
 const CONJURED = 'Conjured Mana Cake'
+const MIN_QUALITY = 0
+const MAX_QUALITY = 50
 
 
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
+
   updateQuality() {
     for (const item of this.items) {
-      if (item.name != BRIE && item.name != BACKSTAGE_PASSES) {
-        if (item.quality > 0) {
-          if (item.name != SULFURAS) {
-            item.quality --;
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == BACKSTAGE_PASSES) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality ++;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality ++;
-              }
-            }
-          }
-        }
+      //checking if item is standard and if it is, decrementing quality by 1
+      if (item.name != BRIE && item.name != BACKSTAGE_PASSES && item.name != SULFURAS && item.quality > MIN_QUALITY) {
+        item.quality --;
       }
+       else {
+        //checking if Brie quality is less than 50, then incrementing
+        if (item.quality < MAX_QUALITY) {
+          item.quality ++
+        }
+        //then if it is BACKSTAGE_PASSES and sellIn is less than 11 and quality is still 
+        //less than 50 then increment and then check if the sellIn is less than 6 and
+        //quality is still less them 50 then increment
+        if (item.name == BACKSTAGE_PASSES && item.sellIn < 11 && item.quality < MAX_QUALITY) {
+          item.quality ++
+        if (item.name == BACKSTAGE_PASSES && item.sellIn < 6 && item.quality < MAX_QUALITY) {
+          item.quality ++;
+        }
+          }
+        }
+      
+      //deals with decrementing days which is equal for all items apart from SULFURAS
       if (item.name != SULFURAS) {
         item.sellIn --;
       }
-      if (item.sellIn < 0 || item.name == CONJURED)  {
+      if (item.sellIn < MIN_QUALITY || item.name == CONJURED)  {
         if (item.name != BRIE) {
           if (item.name != BACKSTAGE_PASSES) {
-            if (item.quality > 0) {
+            if (item.quality > MIN_QUALITY) {
               if (item.name != SULFURAS) {
                 item.quality --;
               }
@@ -56,7 +57,7 @@ class Shop {
             item.quality = item.quality - item.quality;
           }
         } else {
-          if (item.quality < 50) {
+          if (item.quality < MAX_QUALITY) {
             item.quality ++;
           }
         }
@@ -66,6 +67,11 @@ class Shop {
     return this.items;
   }
 }
+
+function createShop(items=[]) {
+  return new Shop();
+}
+
 module.exports = {
   Item,
   Shop
