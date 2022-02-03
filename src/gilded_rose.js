603c3fd9ef3a336ef1af1fffd1fb5ed109d84685
Ben Corbin <ben@corbin.digital>
2022-02-03 11:51:39 +0000

    Final changes before creating extended classes from Item and polymorphic method for update quality

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -8,10 +8,13 @@ class Item {
 
 const BRIE = 'Aged Brie'
 const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert'
+const BACKSTAGE_PASSES_ACCELORATOR_1 = 11
+const BACKSTAGE_PASSES_ACCELORATOR_2 = 6
 const SULFURAS = 'Sulfuras, Hand of Ragnaros'
 const CONJURED = 'Conjured Mana Cake'
 const MIN_QUALITY = 0
 const MAX_QUALITY = 50
+const ITEM_SELL_IN_ZERO = 0
 
 
 class Shop {
@@ -31,39 +34,29 @@ class Shop {
           item.quality ++
         }
         //then if it is BACKSTAGE_PASSES and sellIn is less than 11 and quality is still 
-        //less than 50 then increment and then check if the sellIn is less than 6 and
-        //quality is still less them 50 then increment
-        if (item.name == BACKSTAGE_PASSES && item.sellIn < 11 && item.quality < MAX_QUALITY) {
+        //less than 50 then increment by 1
+        if (item.name == BACKSTAGE_PASSES && item.sellIn < BACKSTAGE_PASSES_ACCELORATOR_1 && item.quality < MAX_QUALITY) {
           item.quality ++
-        if (item.name == BACKSTAGE_PASSES && item.sellIn < 6 && item.quality < MAX_QUALITY) {
+          //and then check if the sellIn is less than 6 and
+        //quality is still less them 50 then increment by 1 again
+        if (item.name == BACKSTAGE_PASSES && item.sellIn < BACKSTAGE_PASSES_ACCELORATOR_2 && item.quality < MAX_QUALITY) {
           item.quality ++;
         }
-          }
+        //reduces the quality of BACKSTAGE_PASSES to 0 as soon as they expire
+        if(item.name == BACKSTAGE_PASSES && item.sellIn === ITEM_SELL_IN_ZERO) {
+          item.quality = MIN_QUALITY
+        } 
         }
-      
+      }
       //deals with decrementing days which is equal for all items apart from SULFURAS
       if (item.name != SULFURAS) {
         item.sellIn --;
       }
-      if (item.sellIn < MIN_QUALITY || item.name == CONJURED)  {
-        if (item.name != BRIE) {
-          if (item.name != BACKSTAGE_PASSES) {
-            if (item.quality > MIN_QUALITY) {
-              if (item.name != SULFURAS) {
-                item.quality --;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < MAX_QUALITY) {
-            item.quality ++;
-          }
-        }
-      }
+      //Decrements quality again for items that have expired, or are conjured, and makes sure that these items are not one of the special items
+      if (item.sellIn < MIN_QUALITY || item.name == CONJURED && item.name != BRIE && item.name != BACKSTAGE_PASSES && item.name != SULFURAS)  {
+        item.quality --;
+              }           
     }
-
     return this.items;
   }
 }
